c21ddc187cc4a18011945b093083a99b0e24eb5b
JoaoESmoreira <jesmoreira2010@gmail.com>
2022-05-28 23:08:00 +0100

    simple commit

--- src/main.m
+++ src/main.m
@@ -4,17 +4,11 @@
 %% Question 1
 load_my_data;
 
-
 % initial variables
 axis_name        = ["Axis x" "Axis y" "Axis z"];
 experiences_name = ["Exp54" "Exp55" "Exp56" "Exp57" "Exp58" "Exp59" "Exp60" "Exp61"];
 
 %experiences(:,:,:) = [exp54_user27, exp55_user27, exp56_user28, exp57_user28, exp58_user29, exp59_user29, exp60_user30, exp61_user30];
-
-
-
-
-
 
 
 %% Question 2
@@ -25,3 +19,13 @@ for k = 54:61
         my_plot(data_label, eval(experience), experiences_name(k-53), activities, i, axis_name(i), k)
     end
 end
+
+%% Question 3
+%X = zeros(N - 1);
+X = fftshift(fft(exp54(:,1)));
+
+figure(1),plot(1:length(X), X)
+
+
+
+
